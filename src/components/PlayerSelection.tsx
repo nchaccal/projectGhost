c9dd846e29edccfd9f2498272b3ece802eaa2965
Nicolás Chaccal <nicolaschaccal@gmail.com>
2022-07-11 22:18:01 -0300

    Player select box

--- src/components/PlayerSelection.tsx
+++ src/components/PlayerSelection.tsx
@@ -8,6 +8,7 @@ const PlayerSelection: FC = () => {
     //TO GET THE PUBLIC KEY OF THE CONNECTED WALLET
     const { publicKey } = useWallet();
 
+    //Each time a wallet if connected, execute this code
     useEffect(() => {
 
         (async () => {
@@ -25,7 +26,7 @@ const PlayerSelection: FC = () => {
         
             const owner = new PublicKey(publicKey);
             const allNFTs = await metaplex.nfts().findAllByOwner(owner);
-        
+            const justGhosts = await metaplex.nfts().findAllByCreator
             console.log(allNFTs);
         })();
 
@@ -33,8 +34,17 @@ const PlayerSelection: FC = () => {
 
     return (
         <div>
-            <h2>"Its working"</h2>
-        </div>
+        { //IF PUBLIC KEY EXIST, SHOW THE PLAYER SELECTION BOX
+        (publicKey) &&
+            <div style={{ position: 'absolute', top:'50%', left:'50%', transform: 'translate(-50%, -50%)', backgroundColor:'#10141f', color:'white', borderRadius:'4px', minHeight:'70%', width:'70%', padding:'2rem', }}>
+                <div className='HeaderBox'>
+                    <h1  style={{}}> Select your Player!</h1>
+                    <p  style={{}}> You can change your player whenever you want within the game too</p>
+                </div>
+            </div>
+        }
+       
+       </div>
     );
 };
 
